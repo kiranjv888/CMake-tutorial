58a31540c40597f8ccf66850d85009d4c9eee39e
sai kiran jv <kiranjv888@gmail.com>
2018-08-16 20:37:04 +0530

    Update cross_compile.sh

--- cross_compilation_sample_project/cross_compile.sh
+++ cross_compilation_sample_project/cross_compile.sh
@@ -3,7 +3,3 @@ export TOOLCHAIN_DIR=$STAGING_DIR/toolchain-arm_cortex-a7_gcc-4.8-linaro_uClibc-
 export LDCFLAGS=$TOOLCHAIN_DIR/usr/lib
 export LD_LIBRARY_PATH=$TOOLCHAIN_DIR/usr/lib
 export PATH=$TOOLCHAIN_DIR/bin:$PATH
-
-#./configure --prefix=$TOOLCHAIN_DIR --build=arm-openwrt-linux --host=arm-openwrt-linux-ulibcgnueabi
-#make
-#make install
